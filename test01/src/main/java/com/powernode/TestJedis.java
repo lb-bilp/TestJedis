a9e15214d23807de024e3c7d93c0066b1aca44ec
zhangsan <mypostbox2020@163.com>
2022-11-25 20:59:51 +0800

    0

---
+++ test01/src/main/java/com/powernode/TestJedis.java
@@ -0,0 +1,21 @@
+package com.powernode;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * @author
+ * @PackageName:com.powernode
+ * @ClassName:TestJedis
+ * @Description:
+ * @date:2022/11/23 11:26
+ */
+public class TestJedis {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.253.130",6379);
+        jedis.auth("123456");
+        String replication = jedis.info("replication");
+        System.out.println(replication);
+        System.out.println("11111112221sss");
+        System.out.println("aaaaabbb");
+    }
+}
